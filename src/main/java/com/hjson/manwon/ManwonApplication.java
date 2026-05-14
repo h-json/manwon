f4aa50b174a5ded5970b091a7e8480b58bf5357a
h-json <h.json248@gmail.com>
2026-05-14 18:32:57 +0900

    프로젝트 세팅

---
+++ src/main/java/com/hjson/manwon/ManwonApplication.java
@@ -0,0 +1,13 @@
+package com.hjson.manwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManwonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjson/manwon/ManwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjson.manwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
